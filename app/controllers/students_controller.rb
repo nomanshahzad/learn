10438b5dd52c2d1b235874e89a1f27cd188be297
Noman Shahzad <nshahzad560@gmail.com>
2022-04-23 14:31:15 +0500

    Add,Edit & show done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,12 +5,42 @@ class StudentsController < ApplicationController
 
     def create
         @student = Student.new(student_params)
-        @student.save
-        redirect_to student_path(@student)
+        if @student.save
+            flash[:notice] = "Student is Added"
+            redirect_to student_path(@student)
+        else
+            flash[:notice] = "Error"
+            redirect_to new_student_path
+        end
+    end
+
+    def edit
+        @student = Student.find(params[:id])
+    end
+
+    def update
+        @student = Student.find(params[:id])
+        if @student.update(student_params)
+            flash[:notice] = "Student is Updated"
+            redirect_to student_path(@student)
+        else
+            flash[:notice] = "Error"
+            redirect_to new_student_path
+        end
+    end
+
+    def show
+        @student = Student.find(params[:id])
+    end
+
+    def index
+        @student = Student.all
     end
 
     private
     def student_params
         params.require(:student).permit(:name,:roll_num,:father)
     end
+
+
 end

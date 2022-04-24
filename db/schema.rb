75170fe7cc5f685aa327a377164b342e76461505
Noman Shahzad <nshahzad560@gmail.com>
2022-04-24 15:21:26 +0500

    create user

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_20_091721) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_24_101509) do
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "roll_num"
     t.string "father"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
